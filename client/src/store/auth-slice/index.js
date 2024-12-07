a0adf6161f77660c263abd13195d708fead0333b
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-07 10:45:15 +0400

    add registeruser function in auth slice

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import axiosObj from "../../lib/axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isAuthenticated: false,
@@ -6,11 +7,35 @@ const initialState = {
   user: null,
 };
 
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+  async (formData) => {
+    const response = await axiosObj.post("/auth/register", formData);
+    return response.data;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
   reducers: {
     setUser: (state, action) => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(registerUser.rejected, (state) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      });
   },
 });
 
