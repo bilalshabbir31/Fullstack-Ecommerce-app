082c5b025de8886ace844f98779dfca05262eb36
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-07 13:18:31 +0400

    complete login page end to end

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,6 +1,9 @@
 import CommonForm from "@/components/common/Form";
 import { loginFormControls } from "@/config";
+import { useToast } from "@/hooks/use-toast";
+import { loginUser } from "@/store/auth-slice";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 const initialState = {
@@ -8,10 +11,26 @@ const initialState = {
   password: ''
 }
 
-const onSubmit = () => { }
-
 const AuthLogin = () => {
   const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(loginUser(formData)).then(data => {
+      if (data?.payload.success) {
+        toast({
+          title: data?.payload?.message
+        })
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive"
+        })
+      }
+    })
+  }
 
   return (
     <div className="mx-auto w-full max-w-md space-y-6">
