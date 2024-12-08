496e0030a9f68b039e49fecd5d931d697d730921
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-08 11:31:15 +0400

    add product crud routes

--- server/routes/admin/productsRoutes.js
+++ server/routes/admin/productsRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { handleImageUpload } from "../../controllers/admin/productsController.js";
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  fetchAllProduct,
+  handleImageUpload,
+} from "../../controllers/admin/productsController.js";
 import { upload } from "../../utils/imageUploader.js";
 
 const router = express.Router();
 
-router.post('/upload-image', upload.single('my_file'), handleImageUpload)
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/", addProduct);
+router.get("/", fetchAllProduct);
+router.put("/:id", editProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
