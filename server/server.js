d05ba035fbcb1e1674e6b5fd1d27aa36ef9f7477
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-08 10:43:08 +0400

    add image upload route

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
-import authRouter from './routes/auth/authRoutes.js'
+import authRouter from "./routes/auth/authRoutes.js";
+import productRouter from "./routes/admin/productsRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 // routes
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products/", productRouter);
 
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
