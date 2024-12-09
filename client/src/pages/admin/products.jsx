7d94ab7c35c0c0fd7190c325e197574ec40e076f
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-09 17:13:12 +0400

    display all products on product page

--- client/src/pages/admin/products.jsx
+++ client/src/pages/admin/products.jsx
@@ -3,7 +3,11 @@ import CommonForm from "@/components/common/Form";
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
 import { addProductFormElements } from "@/config";
-import { useState } from "react"
+import { useToast } from "@/hooks/use-toast";
+import { addNewProduct, fetchAllProducts } from "@/store/admin/product-slice";
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import AdminProductTile from "./productTile";
 
 const initialFormData = {
   image: null,
@@ -24,15 +28,44 @@ const AdminProducts = () => {
   const [imageFile, setImageFile] = useState(null);
   const [uploadedImageUrl, setUploadedImageUrl] = useState('');
   const [imageLoadingState, setImageLoadingState] = useState(false);
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.adminProducts.products)
+  console.log(products);
 
-  function onSubmit() { }
+  const { toast } = useToast();
+
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(addNewProduct({
+      ...formData,
+      image: uploadedImageUrl
+    })).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchAllProducts())
+        setOpenCreateProductsDialog(false);
+        setImageFile(null)
+        setFormData(initialFormData)
+        toast({
+          title: 'Product Added Successfully!'
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    dispatch(fetchAllProducts())
+  }, [dispatch])
 
   return (
     <>
       <div className="mb-5 w-full flex justify-end">
         <Button onClick={() => setOpenCreateProductsDialog(true)}>Add New Product</Button>
       </div>
-      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {
+          products && products.length > 0 ? products.map((product) => <AdminProductTile key={product._id} product={product} />) : null
+        }
+      </div>
       <Sheet open={openCreateProductsDialog} onOpenChange={() => {
         setOpenCreateProductsDialog(false);
       }}>
@@ -40,7 +73,7 @@ const AdminProducts = () => {
           <SheetHeader>
             <SheetTitle>Add New Product</SheetTitle>
           </SheetHeader>
-          <ProductImageUpload imageFile={imageFile} setImageFile={setImageFile} uploadedImageUrl={uploadedImageUrl} setUploadedImageUrl={setUploadedImageUrl} setImageLoadingState={setImageLoadingState} />
+          <ProductImageUpload imageFile={imageFile} setImageFile={setImageFile} uploadedImageUrl={uploadedImageUrl} setUploadedImageUrl={setUploadedImageUrl} setImageLoadingState={setImageLoadingState} imageLoadingState={imageLoadingState} />
           <div className="py-6">
             <CommonForm onSubmit={onSubmit} formData={formData} setFormData={setFormData} buttonText='Add' formControls={addProductFormElements} />
           </div>
