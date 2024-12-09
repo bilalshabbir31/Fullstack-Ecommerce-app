0534ce0f792e1ebe5ba3e03f868dd7811b4956a4
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-09 14:40:29 +0400

    add admin product slice

---
+++ client/src/store/admin/product-slice/index.js
@@ -0,0 +1,60 @@
+import axiosObj from "@/lib/axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  products: [],
+};
+
+export const addNewProduct = createAsyncThunk(
+  "/products/addNewProduct",
+  async (formData) => {
+    const response = await axiosObj.post("/admin/products/", formData);
+    return response?.data;
+  }
+);
+
+export const fetchAllProducts = createAsyncThunk(
+  "/products/fetchAllProducts",
+  async () => {
+    const response = await axiosObj.get("/admin/products/");
+    return response?.data;
+  }
+);
+export const editProduct = createAsyncThunk(
+  "/products/editProduct",
+  async ({ id, formData }) => {
+    const response = await axiosObj.put(`/admin/products/${id}`, formData);
+    return response?.data;
+  }
+);
+export const deleteProduct = createAsyncThunk(
+  "/products/deleteProduct",
+  async (id) => {
+    const response = await axiosObj.delete(`/admin/products/${id}`);
+    return response?.data;
+  }
+);
+
+const AdminProductSlice = createSlice({
+  name: "adminProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.products = action.payload;
+      })
+      .addCase(fetchAllProducts.rejected, (state) => {
+        state.isLoading = false;
+        state.products = [];
+      });
+  },
+});
+
+
+export default AdminProductSlice.reducer
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
+import AdminProductsReducer from "./admin/product-slice/index";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    adminProducts: AdminProductsReducer
   },
 });
 
