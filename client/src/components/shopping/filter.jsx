1bd5601884aa1d4bc3a466b4217f85d28522fc51
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-13 09:58:33 +0400

    checked filter options

--- client/src/components/shopping/filter.jsx
+++ client/src/components/shopping/filter.jsx
@@ -4,7 +4,7 @@ import { Checkbox } from "../ui/checkbox"
 import { Separator } from "../ui/separator"
 import { Fragment } from "react"
 
-const ProductFilter = () => {
+const ProductFilter = ({ filters, handleFilters }) => {
   return (
     <div className="bg-background rounded-lg shadow-sm">
       <div className="p-4 border-b">
@@ -18,7 +18,9 @@ const ProductFilter = () => {
               <div className="grid gap-2 mt-2">
                 {
                   filterOptions[key].map(option => <Label key={option.id} className="flex font-medium item-center gap-2">
-                    <Checkbox />
+                    <Checkbox checked={
+                      filters && Object.keys(filters).length > 0 && filters[key] && filters[key].indexOf(option.id) > -1
+                    } onCheckedChange={() => handleFilters(key, option.id)} />
                     {option.label}
                   </Label>)
                 }
