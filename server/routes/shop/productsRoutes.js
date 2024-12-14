a8f6670debf3d05a8ed6c1d380e4fdce7b2337fa
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-14 11:36:06 +0400

    add single product endpoint

--- server/routes/shop/productsRoutes.js
+++ server/routes/shop/productsRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { filteredProducts } from "../../controllers/shop/productsController.js";
+import {
+  filteredProducts,
+  product,
+} from "../../controllers/shop/productsController.js";
 
 const router = express.Router();
 
-router.get('/', filteredProducts)
+router.get("/", filteredProducts);
+router.get("/:id", product);
 
 export default router;
