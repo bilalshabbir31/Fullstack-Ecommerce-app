faaef6a79d7855b5d46747ef0532541a70377d9e
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-16 15:00:21 +0400

    add cart reducer in the store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import AdminProductsReducer from "./admin/product-slice/index";
 import ShopProductsReducer from "./shop/product-slice/index";
+import ShopCartReducer from "./shop/cart-slice/index";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: AdminProductsReducer,
     shopProducts: ShopProductsReducer,
+    shopCart: ShopCartReducer,
   },
 });
 
