442b82bfb08d8ca13a6a9d79c3134a9778284c5c
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-16 16:17:32 +0400

    remove receving as object in fetchcartitems

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -20,7 +20,9 @@ export const addToCart = createAsyncThunk(
 
 export const fetchCartItems = createAsyncThunk(
   "cart/fetchCartItems",
-  async ({ userId }) => {
+  async (userId) => {
+    console.log(userId);
+    
     const response = await axiosObj.get(`/shop/cart/${userId}`);
     return response.data;
   }
