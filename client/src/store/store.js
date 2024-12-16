b046793699f694f805843ba3da7d6789d5979930
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-16 14:58:32 +0400

    correct spellings

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import AdminProductsReducer from "./admin/product-slice/index";
-import ShopProducstsReducer from "./shop/product-slice/index";
+import ShopProductsReducer from "./shop/product-slice/index";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: AdminProductsReducer,
-    shopProducts: ShopProducstsReducer,
+    shopProducts: ShopProductsReducer,
   },
 });
 
