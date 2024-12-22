64ca37d68ec5c4c4c6b9937b3959c3b4926b6866
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-22 11:21:18 +0400

    add shopping order component with static data

--- client/src/components/shopping/orders.jsx
+++ client/src/components/shopping/orders.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { Dialog } from "../ui/dialog"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
+import ShoppingOrderDetailsDailog from "./OrderDetailsDailog"
 
-const Orders = () => {
+const ShoppingOrders = () => {
+
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+
   return (
-    <div>Orders</div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Order History</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>12212</TableCell>
+              <TableCell>27/09/2021</TableCell>
+              <TableCell>In Process</TableCell>
+              <TableCell>$1000</TableCell>
+              <TableCell>
+                <Dialog open={openDetailsDialog} onOpenChange={setOpenDetailsDialog}>
+                  <Button onClick={() => setOpenDetailsDialog(true)}>View Details</Button>
+                  <ShoppingOrderDetailsDailog />
+                </Dialog>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   )
 }
 
-export default Orders
+export default ShoppingOrders
