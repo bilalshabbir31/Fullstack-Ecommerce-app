c7427eac2b59e899e3add12cc9b32dad3ea4ddb0
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-22 10:30:23 +0400

    add admin orders component

---
+++ client/src/components/admin/orders.jsx
@@ -0,0 +1,41 @@
+import { Button } from "../ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
+
+const AdminOrders = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Order History</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>12212</TableCell>
+              <TableCell>27/09/2021</TableCell>
+              <TableCell>In Process</TableCell>
+              <TableCell>$1000</TableCell>
+              <TableCell>
+                <Button>View Details</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default AdminOrders
