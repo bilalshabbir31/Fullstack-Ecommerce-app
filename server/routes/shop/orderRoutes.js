e9e2c55eeea33284914cf5cd1da928ec41f83076
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-23 17:07:01 +0400

    add alluserOrders and getOrder endpoint in routes

--- server/routes/shop/orderRoutes.js
+++ server/routes/shop/orderRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import {
+  allUserOrders,
   checkoutSuccess,
   createCheckoutSession,
+  getOrder,
 } from "../../controllers/shop/orderController.js";
 const router = express.Router();
 
 router.post("/create-checkout-session", createCheckoutSession);
 router.post("/checkout-success", checkoutSuccess);
+router.get("/list/:userId", allUserOrders);
+router.get("/id", getOrder);
 
 export default router;
