0514f08a8a256d5b7187a62a95fa1aa019f2e8be
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-23 14:43:36 +0400

    add checkout success route

--- server/routes/shop/orderRoutes.js
+++ server/routes/shop/orderRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { createCheckoutSession } from "../../controllers/shop/orderController.js";
+import {
+  checkoutSuccess,
+  createCheckoutSession,
+} from "../../controllers/shop/orderController.js";
 const router = express.Router();
 
 router.post("/create-checkout-session", createCheckoutSession);
+router.post("/checkout-success", checkoutSuccess);
 
 export default router;
