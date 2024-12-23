48ae4f1405f8fd2b7ea00db66ace54196b3dc103
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-23 10:30:03 +0400

    add order slice

---
+++ client/src/store/shop/order-slice/index.js
@@ -0,0 +1,43 @@
+import axiosObj from "@/lib/axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  sessionId: null,
+  isLoading: false,
+  orderId: null,
+};
+
+export const createNewOrder = createAsyncThunk(
+  "/order/createNewOrder",
+  async (orderData) => {
+    const response = await axiosObj.post(
+      "/shop/order/create-checkout-session",
+      orderData
+    );
+    return response.data;
+  }
+);
+
+const shoppingOrderSlice = createSlice({
+  name: "shoppingOrderSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createNewOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNewOrder.fulfilled, (state, action) => {
+        state.isLoading = false, 
+        state.sessionId = action.payload.sessionId;
+        state.orderId = action.payload.orderId;
+      })
+      .addCase(createNewOrder.rejected, (state) => {
+        state.isLoading = false,
+        state.sessionId = null;
+        state.orderId = null;
+      });
+  },
+});
+
+export default shoppingOrderSlice.reducer;
