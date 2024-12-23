5dd437d70d9303394bb67cb3abc41334eae622a1
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-23 14:23:38 +0400

    correct success and cancel page url

--- server/controllers/shop/orderController.js
+++ server/controllers/shop/orderController.js
@@ -40,8 +40,8 @@ const createCheckoutSession = async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+      success_url: `${process.env.CLIENT_URL}/shop/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URL}/shop/purchase-cancel`,
       metadata: {
         userId: userId,
         products: JSON.stringify(
