6250ecf1234d1ffed7cb9fd9cf1867115355e409
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-24 17:26:14 +0400

    add admin order slice

---
+++ client/src/store/admin/order-slice/index.js
@@ -0,0 +1,60 @@
+import axiosObj from "@/lib/axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  orders: [],
+  order: null,
+};
+
+export const getOrder = createAsyncThunk("/orders/getOrder", async (id) => {
+  const response = await axiosObj.get(`/admin/orders/${id}`);
+  return response.data;
+});
+
+export const fetchAllOrders = createAsyncThunk(
+  "/orders/fetchAllOrders",
+  async () => {
+    const response = await axiosObj.get('/admin/orders/');
+    return response.data;
+  }
+);
+
+const adminOrderSlice = createSlice({
+  name: "AdminOrderSlice",
+  initialState,
+  reducers: {
+    resetOrderDetails: (state) => {
+      state.order = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.order = action.payload.data;
+      })
+      .addCase(getOrder.rejected, (state) => {
+        state.isLoading = false;
+        state.order = null;
+      })
+      .addCase(fetchAllOrders.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllOrders.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orders = action.payload.data;
+      })
+      .addCase(fetchAllOrders.rejected, (state) => {
+        state.isLoading = false;
+        state.orders = [];
+      });
+  },
+});
+
+export const { resetOrderDetails } = adminOrderSlice.actions;
+
+export default adminOrderSlice.reducer;
