d7056ff1feb867b90e49df289c7e79e9e48c98c7
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-24 17:27:27 +0400

    make admin order detail page dynamic

--- client/src/components/admin/OrderDetailsDailog.jsx
+++ client/src/components/admin/OrderDetailsDailog.jsx
@@ -3,14 +3,17 @@ import CommonForm from "../common/Form"
 import { DialogContent } from "../ui/dialog"
 import { Label } from "../ui/label"
 import { Separator } from "../ui/separator"
+import { Badge } from "../ui/badge"
+import { useSelector } from "react-redux"
 
 const initialFormData = {
   status: ''
 }
 
-const AdminOrderDetailsDailog = () => {
+const AdminOrderDetailsDailog = ({ order }) => {
 
   const [formData, setFormData] = useState(initialFormData);
+  const { user } = useSelector(state => state.auth);
 
   function handleUpdateStatus(event) {
     event.preventDefault();
@@ -22,19 +25,29 @@ const AdminOrderDetailsDailog = () => {
         <div className="grid gap-2">
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order ID</p>
-            <Label>1212</Label>
+            <Label>{order?._id}</Label>
           </div>
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order Date</p>
-            <Label>12-Nov-2024</Label>
+            <Label>{order?.orderDate.split('T')[0]}</Label>
           </div>
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order Price</p>
-            <Label>$500</Label>
+            <Label>${order?.totalAmount}</Label>
+          </div>
+          <div className="flex mt-6 items-center justify-between">
+            <p className="font-medium">Payment Method</p>
+            <Label>{order?.paymentMethod}</Label>
+          </div>
+          <div className="flex mt-6 items-center justify-between">
+            <p className="font-medium">Payment Status</p>
+            <Label>{order?.paymentStatus}</Label>
           </div>
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order Status</p>
-            <Label>In Process</Label>
+            <Label>
+              <Badge className={`py-1 px-3 ${order?.orderStatus === 'confirmed' ? 'bg-green-500' : 'bg-black'}`}>{order?.orderStatus}</Badge>
+            </Label>
           </div>
         </div>
         <Separator />
@@ -42,10 +55,13 @@ const AdminOrderDetailsDailog = () => {
           <div className="grid gap-2">
             <div className="font-medium">Order Details</div>
             <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span>Product One</span>
-                <span>$100</span>
-              </li>
+              {
+                order?.cartItems && order?.cartItems.length > 0 ? order?.cartItems.map(item => <li key={item.productId} className="flex items-center justify-between">
+                  <span>Title: {item?.title}</span>
+                  <span>Quantity: {item?.quantity}</span>
+                  <span>Price: ${item?.price}</span>
+                </li>) : null
+              }
             </ul>
           </div>
         </div>
@@ -53,28 +69,36 @@ const AdminOrderDetailsDailog = () => {
           <div className="grid gap-2">
             <div className="font-medium">Shipping Info</div>
             <div className="grid gap-0.5 text-muted-foreground">
-              <span>John Doe</span>
-              <span>Address</span>
-              <span>City</span>
-              <span>Pincode</span>
-              <span>Phone</span>
-              <span>Notes</span>
+              <span>{user?.userName}</span>
+              <span>{order?.addressInfo?.address}</span>
+              <span>{order?.addressInfo?.city}</span>
+              <span>{order?.addressInfo?.pincode}</span>
+              <span>{order?.addressInfo?.phone}</span>
+              <span>{order?.addressInfo?.notes}</span>
             </div>
           </div>
         </div>
         <div>
-          <CommonForm formControls={[{
-            label: "Order Status",
-            name: "status",
-            componentType: "select",
-            options: [
-              { id: "pending", label: "Pending" },
-              { id: "inProcess", label: "In Process" },
-              { id: "inShipping", label: "In Shipping" },
-              { id: "rejected", label: "Rejected" },
-              { id: "delivered", label: "Delivered" },
-            ],
-          }]} formData={formData} setFormData={setFormData} buttonText={'Update Order Status'} onSubmit={handleUpdateStatus} />
+          <CommonForm
+            formControls={[
+              {
+                label: "Order Status",
+                name: "status",
+                componentType: "select",
+                options: [
+                  { id: "pending", label: "Pending" },
+                  { id: "inProcess", label: "In Process" },
+                  { id: "inShipping", label: "In Shipping" },
+                  { id: "delivered", label: "Delivered" },
+                  { id: "rejected", label: "Rejected" },
+                ],
+              },
+            ]}
+            formData={formData}
+            setFormData={setFormData}
+            buttonText={"Update Order Status"}
+            onSubmit={handleUpdateStatus}
+          />
         </div>
       </div>
     </DialogContent>
