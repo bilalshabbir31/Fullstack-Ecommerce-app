460ad2fa69e3f767902fdb27ff5d7d78599434b9
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-25 10:28:25 +0400

    add update order status endpoint

--- server/controllers/admin/orderController.js
+++ server/controllers/admin/orderController.js
@@ -29,4 +29,24 @@ const getOrder = async (req, res) => {
   }
 };
 
-export { fetchAllOrders, getOrder };
+const updateOrderStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const order = await Order.findById(id);
+    if (!order) {
+      return res.status(404).json({ succes: false, message: "No order found" });
+    }
+
+    await Order.findByIdAndUpdate(id, { orderStatus: status });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Order Status Updated Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export { fetchAllOrders, getOrder, updateOrderStatus };
