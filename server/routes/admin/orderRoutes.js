0deda1db00fe55e6ad900e189924b3a701bb72bc
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-25 10:28:09 +0400

    add update order status route

--- server/routes/admin/orderRoutes.js
+++ server/routes/admin/orderRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { fetchAllOrders, getOrder } from "../../controllers/admin/orderController.js";
+import {
+  fetchAllOrders,
+  getOrder,
+  updateOrderStatus,
+} from "../../controllers/admin/orderController.js";
 const router = express.Router();
 
 router.get("/", fetchAllOrders);
 router.get("/:id", getOrder);
+router.put("/:id", updateOrderStatus);
 
 export default router;
