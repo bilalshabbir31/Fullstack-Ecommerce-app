63080af49d05035bf9564bfddf8b7873f692f578
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-29 14:58:04 +0400

    high lighted selected address

--- client/src/components/shopping/addressCard.jsx
+++ client/src/components/shopping/addressCard.jsx
@@ -2,10 +2,10 @@ import { Button } from "../ui/button"
 import { Card, CardContent, CardFooter } from "../ui/card"
 import { Label } from "../ui/label"
 
-const AddressCard = ({ addressInfo, handleDeleteAddress, handleEditAddress, setCurrentSelectedAddress }) => {
+const AddressCard = ({ addressInfo, handleDeleteAddress, handleEditAddress, setCurrentSelectedAddress, selectedId }) => {
   return (
-    <Card onClick={setCurrentSelectedAddress ? () => setCurrentSelectedAddress(addressInfo) : null} >
-      <CardContent className="grid py-4 gap-4">
+    <Card onClick={setCurrentSelectedAddress ? () => setCurrentSelectedAddress(addressInfo) : null} className={`cursor-pointer border-red-700 ${selectedId === addressInfo?._id ? 'border-red-900 border-[2px]' : 'border-black'}`} >
+      <CardContent className={`grid py-4 gap-4 ${selectedId === addressInfo?._id ? 'border black' : ""}`}>
         <Label>Address: {addressInfo?.address}</Label>
         <Label>City: {addressInfo?.city}</Label>
         <Label>PinCode: {addressInfo?.pincode}</Label>
