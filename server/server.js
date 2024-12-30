d677209211eb0557e02acc63f35c68162f2086ec
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-30 16:37:31 +0400

    remove app url and keep it in the .env file

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
