dd9e46030e4f95f85f97b7201b8da4f1fb94d3e9
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-12-30 13:52:18 +0400

    add custom styling in image upload component

--- client/src/components/admin/imageUpload.jsx
+++ client/src/components/admin/imageUpload.jsx
@@ -6,8 +6,8 @@ import { Button } from "../ui/button";
 import axiosObj from "@/lib/axios";
 import { Skeleton } from "../ui/skeleton";
 
-const ProductImageUpload = ({ imageFile, imageLoadingState, setImageFile, uploadedImageUrl, setUploadedImageUrl, setImageLoadingState, isEditMode }) => {
-  
+const ProductImageUpload = ({ imageFile, imageLoadingState, setImageFile, uploadedImageUrl, setUploadedImageUrl, setImageLoadingState, isEditMode, isCustomStyling = false }) => {
+
   const inputRef = useRef(null);
 
   function handleImageFileChange(event) {
@@ -56,16 +56,16 @@ const ProductImageUpload = ({ imageFile, imageLoadingState, setImageFile, upload
 
 
   return (
-    <div className="w-full max-w-md mx-auto mt-4">
+    <div className={`w-full ${isCustomStyling ? '' : 'max-w-md mx-auto mt-4'}`}>
       <Label className="text-lg font-semibold mb-2 block">Upload Image</Label>
-      <div onDragOver={handleDragOver} onDrop={handleDrop} className={`${isEditMode ? 'opacity-60': null} border-2 border-dashed rounded-lg p-4`}>
+      <div onDragOver={handleDragOver} onDrop={handleDrop} className={`${isEditMode ? 'opacity-60' : null} border-2 border-dashed rounded-lg p-4`}>
         <Input id="image-upload" type="file" className="hidden" ref={inputRef} onChange={handleImageFileChange} disabled={isEditMode} />
         {
           !imageFile ?
             <Label htmlFor="image-upload" className={`${isEditMode ? 'cursor-not-allowed' : null}flex flex-col justify-center h-32 cursor-pointer`}>
               <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
               <span>Drag & Drop or Click to Upload Image</span>
-            </Label> : ( imageLoadingState? <Skeleton className='h-10 bg-gray-100' /> :<div className="flex items-center justify-between">
+            </Label> : (imageLoadingState ? <Skeleton className='h-10 bg-gray-100' /> : <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <FileIcon className="w-8 h-8 text-primary mr-2" />
               </div>
